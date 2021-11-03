44e9b6201762e8c128955d09e90d9718975121e3
Nanogamexe <47512743+Nanogamexe@users.noreply.github.com>
2021-11-03 16:09:03 -0400

    almost done

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let compte = 0
+input.onButtonPressed(Button.A, function () {
+    compte = compte + 1
+    basic.showNumber(compte)
+})
+input.onButtonPressed(Button.B, function () {
+    compte = compte - 1
+    basic.showNumber(compte)
+})
 basic.forever(function () {
-	
+    if (compte >= 10) {
+        basic.showIcon(IconNames.No)
+    }
+})
+basic.forever(function () {
+    if (compte > 0) {
+        basic.showNumber(0)
+    }
 })
